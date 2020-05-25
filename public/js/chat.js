63d852910fb4544df30b83686d3f6049d52a7aec
Vijay <vijay.chichula@hitachiconsulting.com>
2020-05-26 00:05:03 +0530

    1. Broadcasting welcome message 2. Bradcating user join and leave chat 3. Sending current location 4. Acknowledge message delivery status and location status

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,5 +1,6 @@
 const client = io()
 
+
 client.on('broadcastMsg', (message) => {
     console.log(message)
 })
@@ -8,5 +9,25 @@ document.querySelector('#message-form').addEventListener('submit', (e) => {
     e.preventDefault()
 
     const message = e.target.elements.message.value
-    client.emit('acknowledgeMessage', message)
+    client.emit('acknowledgeMessage', message, (error) => {
+        if (error) {
+            return console.log(error)
+        }
+        console.log("Delivered !!")
+    })
+})
+
+// send current location
+document.querySelector('#send-location').addEventListener('click', (e) => {
+    if (!navigator.geolocation)
+        alert("Geo loaction do not support")
+    navigator.geolocation.getCurrentPosition((position) => {
+        
+        client.emit('geoLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, ()=>{
+            console.log("location shared !")
+        })
+    })
 })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const http = require('http')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,14 +19,35 @@ app.use(express.static(publicDirectoryPath))
 // server (emit) -> client (receive) - countUpdated
 // client (emit) -> server (receive) - increment
 
-let message = 'Welocme !!'
 // enables chat soket to access in web pages
 io.on('connection', (socket) => {
-    console.log("Chat Socket !!")
-    socket.emit('broadcastMsg', message)
+    console.log("new Socket connection initaited !!", socket.id)
+    socket.emit('broadcastMsg', "Welocme !!")
+    socket.broadcast.emit("broadcastMsg", "New user joined " + socket.id)
 
-    socket.on('acknowledgeMessage', (ackMessage)=>{
-        io.emit('broadcastMsg', ackMessage)
+    // recieving client messages
+    socket.on('acknowledgeMessage', (clientMessage, callback) => {
+        // filtering the messages
+        var filter = new Filter()
+        filter.addWords('madarchod', 'bsdk', 'lavda')
+        if (filter.isProfane(clientMessage)) {
+            return callback('bad words !!!!')
+        }
+        io.emit('broadcastMsg', clientMessage)
+
+        // callback is using to ack the msg delivery status
+        callback('Delivered : !')
+    })
+
+    // reciving current location and updating to all clients
+    socket.on('geoLocation', (geoLocation, callback) => {
+        io.emit('broadcastMsg', `https://google.com/maps?q=${geoLocation.latitude},${geoLocation.longitude}`)
+        callback()
+    })
+
+    // broadcast all clients if any client is disconnected
+    socket.on('disconnect', () => {
+        io.emit('broadcastMsg', socket.id + " user left !!")
     })
 })
 
