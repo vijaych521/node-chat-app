a2e8ef805c4c6e67444a38f6e119c229cea579ac
Vijay <vijay.chichula@hitachiconsulting.com>
2020-05-26 23:14:47 +0530

    changing port

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const { generateMessage, generateLocationMessage } = require('./utils/messages')
 const { addUser, getUser, removeUser, getUsersInRoom } = require('./utils/user.js')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 4000
 
 // created socket which can communicate clients
 const server = http.createServer(app)
